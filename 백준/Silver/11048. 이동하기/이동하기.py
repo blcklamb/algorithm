7b43523c3236ab578795948e209a59075e515741
Chaejung Kim <whenucan35@gmail.com>
2023-07-20 22:21:00 +0900

    [Silver II] Title: 이동하기, Time: 620 ms, Memory: 78824 KB -BaekjoonHub

--- 백준/Silver/11048. 이동하기/이동하기.py
+++ 백준/Silver/11048. 이동하기/이동하기.py
@@ -1,18 +1,18 @@
-N, M = map(int, input().split())
+def solution():
+    N, M = map(int, input().split())
+    roomInfo = [[0 for _ in range(M+1)]]
+    for _ in range(N):
+        row = list(map(int, input().split()))
+        roomInfo.append([0] + row)
 
-roomInfo = []
-for _ in range(N):
-    row = list(map(int, input().split()))
-    roomInfo.append(row)
+    DP = [[0 for _ in range(M+1)] for _ in range(N + 1)]
 
-DP = [[0 for _ in range(M)] for _ in range(N)]
+    for r in range(1, N + 1):
+        for c in range(1, M + 1):
+            DP[r][c] = max(DP[r][c-1], DP[r-1][c],
+                           DP[r-1][c-1]) + roomInfo[r][c]
 
-for r in range(N):
-    for c in range(M):
+    return DP[N][M]
 
-        left = DP[r][c-1] if c - 1 >= 0 else 0
-        upper = DP[r-1][c] if r - 1 >= 0 else 0
-        diagonal = DP[r-1][c-1] if c - 1 >= 0 and r - 1 >= 0 else 0
-        DP[r][c] = max(left, upper, diagonal) + roomInfo[r][c]
 
-print(DP[N-1][M-1])
+print(solution())
