23b19ed1a07e71a7b0de818d15384a2cc551a9b4
Chaejung Kim <whenucan35@gmail.com>
2023-07-20 19:42:54 +0900

    [Silver IV] Title: 붙임성 좋은 총총이, Time: 80 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/26069. 붙임성 좋은 총총이/붙임성 좋은 총총이.py
@@ -0,0 +1,27 @@
+
+pplCount = int(input())
+
+pplInfo = {}
+
+
+def addValue(a, b):
+    if a in pplInfo:
+        pplInfo[a].append(b)
+    else:
+        pplInfo[a] = [b]
+    if b in pplInfo:
+        pplInfo[b].append(a)
+    else:
+        pplInfo[b] = [a]
+
+
+for _ in range(pplCount):
+    a, b = input().split()
+    if a == 'ChongChong' or b == 'ChongChong':
+        addValue(a, b)
+        continue
+    if a in pplInfo or b in pplInfo:
+        addValue(a, b)
+        continue
+
+print(len(pplInfo.keys()))
