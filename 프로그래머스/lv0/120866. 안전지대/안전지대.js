5f80ce01802ed22f88f1ee5fb2437f9544144814
Chaejung Kim <whenucan35@gmail.com>
2023-07-25 17:10:18 +0900

    [level 0] Title: 안전지대, Time: 0.46 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120866. 안전지대/안전지대.js
+++ 프로그래머스/lv0/120866. 안전지대/안전지대.js
@@ -1,13 +1,23 @@
 function solution(board) {
-
-    let outside = [[-1,0], [-1,-1], [-1,1], [0,-1],[0,1],[1,0], [1,-1], [1,1]];
-    let safezone = 0;
-
-    board.forEach((row, y, self) => row.forEach((it, x) => {
-        if (it === 1) return false;
-        return outside.some(([oy, ox]) => !!self[oy + y]?.[ox + x])
-               ? false : safezone++;
-    }));
-
-    return safezone;
+  const N = board.length;
+  const dangerChecked = Array.from({ length: N }, () => Array(N).fill(0));
+  const di = [0, -1, -1, 0, 1, 1, 1, 0, -1];
+  const dj = [0, 0, 1, 1, 1, 0, -1, -1, -1];
+  for (let i = 0; i < N; i++) {
+    for (let j = 0; j < N; j++) {
+      if (board[i][j] === 1) {
+        Array.from({ length: 9 }, (_, idx) => idx).forEach((dIdx) => {
+          let [newI, newJ] = [i + di[dIdx], j + dj[dIdx]];
+          if ((0 <= newI) & (newI < N) & (0 <= newJ) & (newJ < N)) {
+            dangerChecked[newI][newJ] = 1;
+          }
+        });
+      }
+    }
+  }
+  const safeAreaCount = dangerChecked.reduce(
+    (a, b) => a + b.filter((cell) => cell === 0).length,
+    0
+  );
+  return safeAreaCount;
 }
