f50c569212e9c5a6835d3b649058ec768df82cf2
Chaejung Kim <whenucan35@gmail.com>
2023-08-02 17:39:02 +0900

    [Gold V] Title: 센서, Time: 48 ms, Memory: 32276 KB -BaekjoonHub

---
+++ 백준/Gold/2212. 센서/센서.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+K = int(input())
+sensorList = sorted(list(map(int, input().split())))
+
+gapList = []
+for idx in range(1, N):
+    gapList.append(sensorList[idx] - sensorList[idx-1])
+
+if K >= N:
+    print(0)
+else:
+    print(sum(sorted(gapList, reverse=True)[(K-1):]))
