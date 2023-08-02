e29c81e8092bbb9bc96f152bcb56c9b6000f1776
Chaejung Kim <whenucan35@gmail.com>
2023-08-02 17:02:56 +0900

    [Silver I] Title: 음하철도 구구팔, Time: 40 ms, Memory: 33376 KB -BaekjoonHub

--- 백준/Silver/1393. 음하철도 구구팔/음하철도 구구팔.py
+++ 백준/Silver/1393. 음하철도 구구팔/음하철도 구구팔.py
@@ -1,20 +1,35 @@
+import math
+
 xs, ys = map(int, input().split())
 xe, ye, dx, dy = map(int, input().split())
 
-distance = (xs - xe)**2 + (ys - ye)**2
-
-after1SecDistance = (xe + dx - xs)**2 + (ye + dy - ys)**2
-
-if after1SecDistance > distance:
+if dx == 0 and dy == 0:
     print(xe, ye)
-elif dx == 0 and dy == 0:
-    print(xe, ye)
-else:
+    exit()
+
+def getDeg(v1, v2):
+    sizeV1 = math.sqrt(v1[0]**2 + v1[1]**2)
+    sizeV2 = math.sqrt(v2[0]**2 + v2[1]**2)
+
+    # v1*v2
+    innerProduct = v1[0]*v2[0] + v1[1]*v2[1]
+
+    # |v1|*|v2|
+    sizeMultiply = sizeV1 * sizeV2
+
+    angleDeg = math.degrees(math.acos(innerProduct/sizeMultiply))
+    return angleDeg
+
+angleDeg = getDeg([xs-xe, ys-ye], [dx, dy])
+
+if angleDeg < 90:
     if dy == 0:
         print(xs, ye)
     elif dx == 0:
         print(xe, ys)
     else:
-        shortestX = ((dx*dy)/(dy**2 + dx**2))*(dx/dy*xs + ys + dy/dx*xe - ye)
+        shortestX = ((dx*dy)/(dy**2 + dx**2)) * (dx/dy*xs + ys + dy/dx*xe - ye)
         shortestY = (dy/dx)*shortestX - (dy/dx*xe) + ye
         print(int(shortestX), int(shortestY))
+else:
+    print(xe, ye)
