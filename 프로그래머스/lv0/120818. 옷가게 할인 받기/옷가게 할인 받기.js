3fe29909a70c0f067d0df8d223853ca60cfbb14c
Chaejung Kim <whenucan35@gmail.com>
2023-08-03 22:17:19 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.js
@@ -0,0 +1,10 @@
+function solution(price) {
+    if (price >= 500_000){
+        return parseInt(price * 0.8)
+    } else if (price >= 300_000){
+        return parseInt(price * 0.9)
+    } else if (price >= 100_000){
+        return parseInt(price * 0.95)
+    }
+    return price;
+}
