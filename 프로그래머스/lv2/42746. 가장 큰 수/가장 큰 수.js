825b642265fa16bd3464ffe4e68e3c6239f46e3b
Chaejung Kim <whenucan35@gmail.com>
2023-08-06 19:09:58 +0900

    [level 2] Title: 가장 큰 수, Time: 0.06 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.js
@@ -0,0 +1,11 @@
+function solution(numbers) {
+    numbers.sort((a, b) => {
+        const AB = a.toString() + b.toString()
+        const BA = b.toString() + a.toString()
+        return parseInt(BA) - parseInt(AB)
+    })
+    if (numbers[0] === 0){
+        return '0'
+    }
+    return numbers.join('').toString()
+}
