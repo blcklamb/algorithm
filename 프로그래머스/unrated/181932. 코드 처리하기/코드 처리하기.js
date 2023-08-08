c5b8cf0da3593db44d2908863c11f005695a94e9
Chaejung Kim <whenucan35@gmail.com>
2023-08-08 16:58:07 +0900

    [unrated] Title: 코드 처리하기, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181932. 코드 처리하기/코드 처리하기.js
+++ 프로그래머스/unrated/181932. 코드 처리하기/코드 처리하기.js
@@ -2,12 +2,19 @@ function solution(code) {
     let ret = '';
     let mode = 0;
     [...code].forEach((ele, idx) => {
-        if (+ele){
-            mode = Math.abs(mode - 1)
-            return
-        } 
-        if (!mode) ret += (idx % 2 === 0) ? ele : ''
-        if (mode) ret += (idx % 2 != 0 ) ? ele : ''
+        if (!mode){
+            if (ele === '1'){
+                mode = 1
+            } else {
+                ret += (idx % 2 === 0) ? ele : ''
+            }
+        } else {
+            if (ele === '1'){
+                mode = 0
+            } else {
+                ret += (idx % 2 != 0 ) ? ele : ''
+            }
+        }
     })
     return ret.length != 0 ? ret : 'EMPTY';
 }
