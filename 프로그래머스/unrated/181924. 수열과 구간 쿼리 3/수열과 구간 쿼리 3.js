f47118996d2159f65508130b9d5e07248906c8e9
Chaejung Kim <whenucan35@gmail.com>
2023-08-17 22:38:09 +0900

    [unrated] Title: 수열과 구간 쿼리 3, Time: 0.40 ms, Memory: 33.8 MB -BaekjoonHub

--- 프로그래머스/unrated/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.js
+++ 프로그래머스/unrated/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.js
@@ -1,9 +1,6 @@
 function solution(arr, queries) {
-    queries.forEach((ele) => {
-        const [a, b] = ele
-        const [prevA, prevB] = [arr[a], arr[b]]
-        arr[a] = prevB
-        arr[b] = prevA
+    queries.forEach(([a, b]) => {
+        [arr[a], arr[b]] = [arr[b], arr[a]]
     })
     return arr;
 }
