136aa0926121d5cae1cd5887d7d4ac1010dec483
blcklamb <whenucan35@gmail.com>
2023-08-18 15:46:54 +0900

    [lv0] Title: 수열과 구간 쿼리 4, -직접코드추가, 결과추가

--- 프로그래머스/unrated/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.js
+++ 프로그래머스/unrated/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.js
@@ -1,9 +1,42 @@
 function solution(arr, queries) {
-    const N = arr.length
-    queries.forEach(([s, e, k]) => {
-        Array.from({length: N}, (_, idx) => idx)
-            .filter(ele => s <= ele && ele <= e && ele % k == 0)
-            .forEach(ele => arr[ele] += 1)  
-    })
-    return arr;
+  const N = arr.length;
+  queries.forEach(([s, e, k]) => {
+    Array.from({ length: N }, (_, idx) => idx)
+      .filter((ele) => s <= ele && ele <= e && ele % k == 0)
+      .forEach((ele) => (arr[ele] += 1));
+  });
+  return arr;
 }
+// 결과
+```
+'평균 시간: 29.040ms'
+'평균 메모리: 35.982MB'
+'최고 시간: 72.6ms'
+'최저 시간: 0.23ms'
+'최고 메모리: 37.7MB'
+'최저 메모리: 33.6MB'
+'시간 표준 편차: 979.395'
+'메모리 표준 편차: 18.308'
+```;
+
+function solution(arr, queries) {
+  queries.forEach(([s, e, k]) => {
+    const leastPN = Math.ceil(s / k) * k;
+    for (let i = leastPN; i <= e; i += k) {
+      arr[i] += 1;
+    }
+  });
+  return arr;
+}
+
+// 결과
+```
+'평균 시간: 0.288ms'
+'평균 메모리: 33.700MB'
+'최고 시간: 0.44ms'
+'최저 시간: 0.06ms'
+'최고 메모리: 34.1MB'
+'최저 메모리: 33.2MB'
+'시간 표준 편차: 0.148'
+'메모리 표준 편차: 10.475'
+```;
