d3878db6f42ac68c171ccb3700b5b234e245fb81
blcklamb <whenucan35@gmail.com>
2023-08-18 15:36:24 +0900

    [lv0] Title: 마지막 두 원소, -우수코드추가

--- 프로그래머스/unrated/181927. 마지막 두 원소/마지막 두 원소.js
+++ 프로그래머스/unrated/181927. 마지막 두 원소/마지막 두 원소.js
@@ -1,12 +1,18 @@
 function solution(num_list) {
-    var answer = [];
-    const LEN = num_list.length
-    const LAST = num_list[LEN-1]
-    const SECOND_LAST = num_list[LEN-2]
-    if (LAST > SECOND_LAST) {
-        num_list.push(LAST - SECOND_LAST)
-    } else {
-        num_list.push(LAST * 2)
-    }
-    return num_list;
+  var answer = [];
+  const LEN = num_list.length;
+  const LAST = num_list[LEN - 1];
+  const SECOND_LAST = num_list[LEN - 2];
+  if (LAST > SECOND_LAST) {
+    num_list.push(LAST - SECOND_LAST);
+  } else {
+    num_list.push(LAST * 2);
+  }
+  return num_list;
+}
+
+// 우수 코드
+function solution(num_list) {
+  const [a, b] = [...num_list].reverse();
+  return [...num_list, a > b ? a - b : a * 2];
 }
