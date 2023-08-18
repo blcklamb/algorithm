04f492daa64806636a1d08e95ac89f6f24e9ea21
blcklamb <whenucan35@gmail.com>
2023-08-18 16:18:03 +0900

    [lv0] Title: 배열 만들기 4, -결과추가

--- 프로그래머스/unrated/181918. 배열 만들기 4/배열 만들기 4.js
+++ 프로그래머스/unrated/181918. 배열 만들기 4/배열 만들기 4.js
@@ -1,14 +1,26 @@
 function solution(arr) {
-    var stk = [];
-    let i = 0
-    while (true){
-        if (i >= arr.length) break
-        if (stk.length != 0 && stk[stk.length -1] >= arr[i]){
-            stk.pop()
-            continue;
-        }
-        stk.push(arr[i]);
-        i++;
+  var stk = [];
+  let i = 0;
+  while (true) {
+    if (i >= arr.length) break;
+    if (stk.length != 0 && stk[stk.length - 1] >= arr[i]) {
+      stk.pop();
+      continue;
     }
-    return stk;
+    stk.push(arr[i]);
+    i++;
+  }
+  return stk;
 }
+
+// 결과
+```
+'평균 시간: 3.518ms'
+'평균 메모리: 38.091MB'
+'최고 시간: 4.04ms'
+'최저 시간: 2.81ms'
+'최고 메모리: 39.2MB'
+'최저 메모리: 37MB'
+'시간 표준 편차: 1.540'
+'메모리 표준 편차: 12.652'
+```;
