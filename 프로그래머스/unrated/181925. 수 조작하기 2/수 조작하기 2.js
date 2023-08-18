12844c7a7b97ac01f3f3f1a041cfe366eacc7ce7
blcklamb <whenucan35@gmail.com>
2023-08-18 15:35:39 +0900

    [lv0] Title: 수 조작하기 2, -우수코드추가

--- 프로그래머스/unrated/181925. 수 조작하기 2/수 조작하기 2.js
+++ 프로그래머스/unrated/181925. 수 조작하기 2/수 조작하기 2.js
@@ -1,10 +1,29 @@
 function solution(numLog) {
-    return numLog.map((ele, idx) => {
-        if (idx === numLog.length-1) return
-        const diff = numLog[idx+1] - ele
-        if (diff === 1) return 'w'
-        if (diff === -1) return 's'
-        if (diff === 10) return 'd'
-        if (diff === -10) return 'a'
-    }).join('');
+  return numLog
+    .map((ele, idx) => {
+      if (idx === numLog.length - 1) return;
+      const diff = numLog[idx + 1] - ele;
+      if (diff === 1) return "w";
+      if (diff === -1) return "s";
+      if (diff === 10) return "d";
+      if (diff === -10) return "a";
+    })
+    .join("");
+}
+
+// 우수 코드
+function solution(numLog) {
+  const convert = {
+    1: "w",
+    "-1": "s",
+    10: "d",
+    "-10": "a",
+  };
+
+  return numLog
+    .slice(1)
+    .map((v, i) => {
+      return convert[v - numLog[i]];
+    })
+    .join("");
 }
