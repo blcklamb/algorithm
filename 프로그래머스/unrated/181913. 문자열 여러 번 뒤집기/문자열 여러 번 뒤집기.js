f9d5682db0e46e140b597b154dd9423c0933ea77
Chaejung Kim <whenucan35@gmail.com>
2023-08-21 00:55:56 +0900

    [unrated] Title: 문자열 여러 번 뒤집기, Time: 45.85 ms, Memory: 34.9 MB -BaekjoonHub

--- 프로그래머스/unrated/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.js
+++ 프로그래머스/unrated/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.js
@@ -1,6 +1,6 @@
 function solution(my_string, queries) {
     return queries.reduce((a, [s, e]) => {
         const aStr = [...a]
-        return [...aStr.slice(0, s), ...aStr.slice(s, e+1).reverse(), ...aStr.slice(e+1, aStr.length)].join('')
+        return `${aStr.slice(0, s).join('')}${aStr.slice(s, e+1).reverse().join('')}${aStr.slice(e+1, aStr.length).join('')}`
     }, my_string)
 }
