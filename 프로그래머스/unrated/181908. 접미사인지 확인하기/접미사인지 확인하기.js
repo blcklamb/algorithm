3db876ac263ce5791424b1562a392e20fc7ba897
Chaejung Kim <whenucan35@gmail.com>
2023-08-21 01:13:18 +0900

    [unrated] Title: 접미사인지 확인하기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181908. 접미사인지 확인하기/접미사인지 확인하기.js
@@ -0,0 +1,9 @@
+function solution(my_string, is_suffix) {
+    var answer = 1;
+    const reversed_compare = [...my_string].reverse()
+    const reversed_target = [...is_suffix].reverse()
+    reversed_target.forEach((ele, idx) => {
+        if (ele != reversed_compare[idx]) answer = 0; return false;
+    })
+    return answer;
+}
