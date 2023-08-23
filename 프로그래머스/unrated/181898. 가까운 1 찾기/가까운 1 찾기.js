88e3bfa46787d419fc4c7ecf01d4a0336eef1b80
blcklamb <whenucan35@gmail.com>
2023-08-23 17:21:33 +0900

    [lv0] Title: 가까운 1 찾기, -우수 코드 추가

--- 프로그래머스/unrated/181898. 가까운 1 찾기/가까운 1 찾기.js
+++ 프로그래머스/unrated/181898. 가까운 1 찾기/가까운 1 찾기.js
@@ -1,3 +1,6 @@
 function solution(arr, idx) {
-    return arr.map((ele, arrIdx) => (arrIdx<idx) ? 0 : ele).indexOf(1);
+  return arr.map((ele, arrIdx) => (arrIdx < idx ? 0 : ele)).indexOf(1);
 }
+
+// 우수 코드
+const solution = (a, i) => a.indexOf(1, i);
