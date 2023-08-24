f5e7f5dd25d6ff87aa46a97c8754df5f066b45f6
blcklamb <whenucan35@gmail.com>
2023-08-24 22:56:41 +0900

    [lv0] Title: 접두사인지 확인하기, -우수코드추가

--- 프로그래머스/unrated/181906. 접두사인지 확인하기/접두사인지 확인하기.js
+++ 프로그래머스/unrated/181906. 접두사인지 확인하기/접두사인지 확인하기.js
@@ -1,7 +1,17 @@
 function solution(my_string, is_prefix) {
-    if (my_string.length < is_prefix.length) return 0
-    for (let i = 0; i < is_prefix.length; i++){
-        if (is_prefix[i] != my_string[i]) return 0
-    }
-    return 1
+  if (my_string.length < is_prefix.length) return 0;
+  for (let i = 0; i < is_prefix.length; i++) {
+    if (is_prefix[i] != my_string[i]) return 0;
+  }
+  return 1;
+}
+
+// 우수 코드
+function solution(my_string, is_prefix) {
+  return my_string.slice(0, is_prefix.length) === is_prefix ? 1 : 0;
+}
+
+// 우수 코드
+function solution(my_string, is_prefix) {
+  return +my_string.startsWith(is_prefix);
 }
