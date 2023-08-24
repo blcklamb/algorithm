31a4467ca0a3044856f53eca89184f4b0b509166
blcklamb <whenucan35@gmail.com>
2023-08-24 22:53:49 +0900

    [lv0] Title: qr code, -우수코드추가

--- 프로그래머스/unrated/181903. qr code/qr code.js
+++ 프로그래머스/unrated/181903. qr code/qr code.js
@@ -1,5 +1,12 @@
 function solution(q, r, code) {
-    return Array
-        .from({length: Math.ceil(code.length / q)}, (_, idx) => q*idx + r)
-        .reduce((a, b) => a + (code[b] || ''), '');
+  return Array.from(
+    { length: Math.ceil(code.length / q) },
+    (_, idx) => q * idx + r
+  ).reduce((a, b) => a + (code[b] || ""), "");
+}
+
+// 우수 코드
+
+function solution(q, r, code) {
+  return [...code].filter((_, i) => i % q === r).join("");
 }
