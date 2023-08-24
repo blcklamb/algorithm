764ad737971799dfc2c41acd414771d4587cba8c
blcklamb <whenucan35@gmail.com>
2023-08-24 22:53:22 +0900

    [lv0] Title: 문자 개수 세기, -우수코드추가

--- 프로그래머스/unrated/181902. 문자 개수 세기/문자 개수 세기.js
+++ 프로그래머스/unrated/181902. 문자 개수 세기/문자 개수 세기.js
@@ -1,7 +1,15 @@
 function solution(my_string) {
-    const A = 65
-    const charArr = Array.from({length: 58}).fill(0);
-    [...my_string].forEach(ele =>  charArr[ele.charCodeAt(0)-A] += 1);
-    charArr.splice(26, 6)
-    return charArr;
+  const A = 65;
+  const charArr = Array.from({ length: 58 }).fill(0);
+  [...my_string].forEach((ele) => (charArr[ele.charCodeAt(0) - A] += 1));
+  charArr.splice(26, 6);
+  return charArr;
+}
+
+// 우수코드
+function solution(str) {
+  return [...str].reduce(
+    (p, c) => (p[c.charCodeAt() - (c === c.toLowerCase() ? 71 : 65)]++, p),
+    Array(52).fill(0)
+  );
 }
