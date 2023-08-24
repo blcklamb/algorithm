16a73cf16f555206e62ad63a2df500d3b8424d50
blcklamb <whenucan35@gmail.com>
2023-08-24 22:57:07 +0900

    [lv0] Title: 접미사인지 확인하기, -우수코드추가

--- 프로그래머스/unrated/181908. 접미사인지 확인하기/접미사인지 확인하기.js
+++ 프로그래머스/unrated/181908. 접미사인지 확인하기/접미사인지 확인하기.js
@@ -1,9 +1,13 @@
 function solution(my_string, is_suffix) {
-    var answer = 1;
-    const reversed_compare = [...my_string].reverse()
-    const reversed_target = [...is_suffix].reverse()
-    reversed_target.forEach((ele, idx) => {
-        if (ele != reversed_compare[idx]) answer = 0; return false;
-    })
-    return answer;
+  var answer = 1;
+  const reversed_compare = [...my_string].reverse();
+  const reversed_target = [...is_suffix].reverse();
+  reversed_target.forEach((ele, idx) => {
+    if (ele != reversed_compare[idx]) answer = 0;
+    return false;
+  });
+  return answer;
 }
+
+// 우수 코드
+const solution = (str, suff) => (str.endsWith(suff) ? 1 : 0);
