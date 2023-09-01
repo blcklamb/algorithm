57872bc687a96c845bebf9299ef2565efa239e63
Chaejung Kim <whenucan35@gmail.com>
2023-09-02 01:06:39 +0900

    [unrated] Title: 배열 만들기 6, Time: 22.06 ms, Memory: 38.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181859. 배열 만들기 6/배열 만들기 6.js
+++ 프로그래머스/unrated/181859. 배열 만들기 6/배열 만들기 6.js
@@ -1,5 +1,4 @@
 function solution(arr) {
-    var answer = [];
     const result = arr.reduce((prev, curr, i) => {
         if (prev.length === 0) return [curr]
         if (prev[prev.length -1] === curr) return prev.slice(0, prev.length-1)
