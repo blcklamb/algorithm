30331cae25d701bafa40e1d5716c242e549fa919
blcklamb <whenucan35@gmail.com>
2023-09-02 01:26:17 +0900

    [lv 0] Title: 무작위로 K개의 수 뽑기, -코드저장

--- 프로그래머스/unrated/181858. 무작위로 K개의 수 뽑기/무작위로 K개의 수 뽑기.js
+++ 프로그래머스/unrated/181858. 무작위로 K개의 수 뽑기/무작위로 K개의 수 뽑기.js
@@ -1,12 +1,15 @@
 function solution(arr, k) {
-    var answer = arr.reduce((prev, curr, _, redArr) => {
-        if (prev.length === k) {redArr.splice(1); return prev}
-        if (prev.length === 0) return [curr]
-        if (!prev.includes(curr)) return [...prev, curr]
-        return prev
-    }, [])
-    while (answer.length < k){
-        answer.push(-1)
+  var answer = arr.reduce((prev, curr, _, redArr) => {
+    if (prev.length === k) {
+      redArr.splice(1);
+      return prev;
     }
-    return answer;
+    if (prev.length === 0) return [curr];
+    if (!prev.includes(curr)) return [...prev, curr];
+    return prev;
+  }, []);
+  while (answer.length < k) {
+    answer.push(-1);
+  }
+  return answer;
 }
