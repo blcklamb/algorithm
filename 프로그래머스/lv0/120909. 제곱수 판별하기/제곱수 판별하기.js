e524a5219e0a2de079780881b7823afbb8994709
blcklamb <whenucan35@gmail.com>
2023-09-03 00:56:17 +0900

    [level 0] Title: 제곱수 판별하기, -우수코드추가

--- 프로그래머스/lv0/120909. 제곱수 판별하기/제곱수 판별하기.js
+++ 프로그래머스/lv0/120909. 제곱수 판별하기/제곱수 판별하기.js
@@ -1,4 +1,9 @@
 function solution(n) {
-    const square = n**(1/2)
-    return square === parseInt(square) ? 1 : 2
+  const square = n ** (1 / 2);
+  return square === parseInt(square) ? 1 : 2;
+}
+
+//우수 코드
+function solution(n) {
+  return Number.isInteger(Math.sqrt(n)) ? 1 : 2;
 }
