8d7d141858038f6dfdbf9793e7ba94bc6eb0d26c
blcklamb <whenucan35@gmail.com>
2023-09-02 15:41:40 +0900

    [lv 0] Title: 1로 만들기, -우수코드추가

--- 프로그래머스/unrated/181880. 1로 만들기/1로 만들기.js
+++ 프로그래머스/unrated/181880. 1로 만들기/1로 만들기.js
@@ -1,10 +1,15 @@
 function solution(num_list) {
-    return num_list.reduce((a, b) => {
-        let count = 0;
-        while (b > 1){
-            b = b % 2 === 0 ? b / 2 : (b-1)/2
-            count += 1
-        }
-        return a + count
-    }, 0)
+  return num_list.reduce((a, b) => {
+    let count = 0;
+    while (b > 1) {
+      b = b % 2 === 0 ? b / 2 : (b - 1) / 2;
+      count += 1;
+    }
+    return a + count;
+  }, 0);
+}
+
+// 우수 코드
+function solution(num_list) {
+  return num_list.map((v) => v.toString(2).length - 1).reduce((a, c) => a + c);
 }
