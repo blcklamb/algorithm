6f676b3c04cb38edca320ed14d1e84f74a82743b
blcklamb <whenucan35@gmail.com>
2023-09-02 16:11:10 +0900

    [lv 0] Title: 날짜 비교하기, -우수코드추가

--- 프로그래머스/unrated/181838. 날짜 비교하기/날짜 비교하기.js
+++ 프로그래머스/unrated/181838. 날짜 비교하기/날짜 비교하기.js
@@ -1,8 +1,11 @@
 function solution(date1, date2) {
-    const [year1, month1, day1] = date1;
-    const [year2, month2, day2] = date2;
-    if (year1 != year2) return year1 < year2 ? 1 : 0
-    if (month1 != month2) return month1 < month2 ? 1 : 0
-    if (day1 != day2) return day1 < day2 ? 1 : 0
-    return 0;
+  const [year1, month1, day1] = date1;
+  const [year2, month2, day2] = date2;
+  if (year1 != year2) return year1 < year2 ? 1 : 0;
+  if (month1 != month2) return month1 < month2 ? 1 : 0;
+  if (day1 != day2) return day1 < day2 ? 1 : 0;
+  return 0;
 }
+
+// 우수 코드
+const solution = (date1, date2) => (new Date(date1) < new Date(date2) ? 1 : 0);
