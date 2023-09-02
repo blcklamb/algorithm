571f5b13d79e7a3f6cf5f45e60a41a80c2ba32ea
blcklamb <whenucan35@gmail.com>
2023-09-02 15:42:07 +0900

    [lv 0] Title: 리스트 자르기, -우수코드추가

--- 프로그래머스/unrated/181897. 리스트 자르기/리스트 자르기.js
+++ 프로그래머스/unrated/181897. 리스트 자르기/리스트 자르기.js
@@ -1,11 +1,24 @@
 function solution(n, slicer, num_list) {
-    const [a, b, c] = slicer;
-    const rangeByNObj = {
-        1:[0, b+1, 1], 
-        2:[a, num_list.length, 1], 
-        3:[a, b+1, 1], 
-        4:[a, b+1, c]
-    };
-    const [start, end, step] = rangeByNObj[n]
-    return num_list.slice(start, end).filter((_, idx) => idx % step === 0);
+  const [a, b, c] = slicer;
+  const rangeByNObj = {
+    1: [0, b + 1, 1],
+    2: [a, num_list.length, 1],
+    3: [a, b + 1, 1],
+    4: [a, b + 1, c],
+  };
+  const [start, end, step] = rangeByNObj[n];
+  return num_list.slice(start, end).filter((_, idx) => idx % step === 0);
+}
+
+// 우수 코드
+const slices = {
+  1: (num_list, [a, b, c]) => num_list.slice(0, b + 1),
+  2: (num_list, [a, b, c]) => num_list.slice(a),
+  3: (num_list, [a, b, c]) => num_list.slice(a, b + 1),
+  4: (num_list, [a, b, c]) =>
+    num_list.slice(a, b + 1).filter((_, i) => i % c === 0),
+};
+
+function solution(n, slicer, num_list) {
+  return slices[n](num_list, slicer);
 }
