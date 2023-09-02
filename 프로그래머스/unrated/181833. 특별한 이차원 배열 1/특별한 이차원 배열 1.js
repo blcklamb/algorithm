a819e7375a9729763298b711e7691537ee224195
blcklamb <whenucan35@gmail.com>
2023-09-02 16:20:41 +0900

    [lv 0] Title: 특별한 이차원 배열 1, -우수코드추가, -결과추가

--- 프로그래머스/unrated/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.js
+++ 프로그래머스/unrated/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.js
@@ -1,6 +1,36 @@
 function solution(n) {
-    return Array
-        .from({length: n}, (_, i) => 
-              Array.from({length: n}, (_, j) => i === j ? 1 : 0)
-             );
+  return Array.from({ length: n }, (_, i) =>
+    Array.from({ length: n }, (_, j) => (i === j ? 1 : 0))
+  );
 }
+// 결과
+```
+'평균 시간: 0.449ms'
+'평균 메모리: 35.780MB'
+'최고 시간: 1ms'
+'최저 시간: 0.07ms'
+'최고 메모리: 38MB'
+'최저 메모리: 33.4MB'
+'시간 표준 편차: 0.399'
+'메모리 표준 편차: 25.350'
+```;
+// 우수코드
+function solution(n) {
+  const answer = Array.from(Array(n), () => Array(n).fill(0));
+
+  for (let i = 0; i < n; i++) {
+    answer[i][i] = 1;
+  }
+  return answer;
+}
+// 결과
+```
+'평균 시간: 0.165ms'
+'평균 메모리: 35.867MB'
+'최고 시간: 0.29ms'
+'최저 시간: 0.05ms'
+'최고 메모리: 38.1MB'
+'최저 메모리: 33.5MB'
+'시간 표준 편차: 0.045'
+'메모리 표준 편차: 25.358'
+```;
