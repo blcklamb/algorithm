1d9ddcd7e3fea61132bfd4b7a4ffdf4dd6f0852d
blcklamb <whenucan35@gmail.com>
2023-09-02 15:41:11 +0900

    [lv 1] Title: A 강조하기, -우수코드추가

--- 프로그래머스/unrated/181874. A 강조하기/A 강조하기.js
+++ 프로그래머스/unrated/181874. A 강조하기/A 강조하기.js
@@ -1,3 +1,8 @@
 function solution(myString) {
-    return [...myString].map(ele => ele === 'a' || ele === 'A' ? 'A': ele.toLowerCase()).join('');
+  return [...myString]
+    .map((ele) => (ele === "a" || ele === "A" ? "A" : ele.toLowerCase()))
+    .join("");
 }
+
+// 우수코드
+const solution = (s) => s.toLowerCase().replaceAll("a", "A");
