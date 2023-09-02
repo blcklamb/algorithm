8b688cf4e87c5dbcf175b3495d34c3fad818f3a1
blcklamb <whenucan35@gmail.com>
2023-09-02 15:40:27 +0900

    [level 0] Title: 인덱스 바꾸기, -우수코드추가

--- 프로그래머스/lv0/120895. 인덱스 바꾸기/인덱스 바꾸기.js
+++ 프로그래머스/lv0/120895. 인덱스 바꾸기/인덱스 바꾸기.js
@@ -1,9 +1,13 @@
 function solution(my_string, num1, num2) {
-    const [a, b] = [my_string[num1], my_string[num2]]
-    return [...my_string]
-        .map((ele, idx) => idx === num2 
-             ? a : 
-             idx === num1 
-             ? b : ele)
-        .join('');
+  const [a, b] = [my_string[num1], my_string[num2]];
+  return [...my_string]
+    .map((ele, idx) => (idx === num2 ? a : idx === num1 ? b : ele))
+    .join("");
+}
+
+// 우수 코드
+function solution(my_string, num1, num2) {
+  my_string = my_string.split("");
+  [my_string[num1], my_string[num2]] = [my_string[num2], my_string[num1]];
+  return my_string.join("");
 }
