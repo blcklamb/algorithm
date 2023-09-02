487edf2ab626a67658436c8373dbb0d9656d2650
blcklamb <whenucan35@gmail.com>
2023-09-02 15:36:35 +0900

    [level 0] Title: 다음에 올 숫자, -우수코드추가

--- 프로그래머스/lv0/120924. 다음에 올 숫자/다음에 올 숫자.js
+++ 프로그래머스/lv0/120924. 다음에 올 숫자/다음에 올 숫자.js
@@ -1,6 +1,15 @@
 function solution(common) {
-    const [a1, a2, a3] = common;
-    return ((a2-a1) === (a3-a2)) 
-        ? 
-        common[common.length-1] + (a2-a1) : a1*((a2/a1)**(common.length))
+  const [a1, a2, a3] = common;
+  return a2 - a1 === a3 - a2
+    ? common[common.length - 1] + (a2 - a1)
+    : a1 * (a2 / a1) ** common.length;
+}
+
+// 우수 코드
+function solution(common) {
+  if (common[1] - common[0] == common[2] - common[1]) {
+    return common.pop() + common[1] - common[0];
+  } else {
+    return (common.pop() * common[1]) / common[0];
+  }
 }
