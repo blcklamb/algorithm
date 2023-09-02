665e45c137be0f4ea8e56748c900d25ee48512f5
blcklamb <whenucan35@gmail.com>
2023-09-02 15:39:28 +0900

    [level 0] Title: 가장 큰 수 찾기, -우수코드추가

--- 프로그래머스/lv0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.js
+++ 프로그래머스/lv0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.js
@@ -1,5 +1,14 @@
 function solution(array) {
-    return array.reduce(([val, valIdx], b, idx) => {
-        return (val < b) ? [b, idx] : [val, valIdx]
-    }, [0, 0]);
+  return array.reduce(
+    ([val, valIdx], b, idx) => {
+      return val < b ? [b, idx] : [val, valIdx];
+    },
+    [0, 0]
+  );
+}
+
+// 우수 코드
+function solution(array) {
+  let max = Math.max(...array);
+  return [max, array.indexOf(max)];
 }
