eb5a439a8fa4c6754fd4b17ba134cc50ff6714dd
Chaejung Kim <whenucan35@gmail.com>
2023-09-02 23:28:20 +0900

    [unrated] Title: 조건에 맞게 수열 변환하기 3, Time: 2.65 ms, Memory: 44.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181835. 조건에 맞게 수열 변환하기 3/조건에 맞게 수열 변환하기 3.js
+++ 프로그래머스/unrated/181835. 조건에 맞게 수열 변환하기 3/조건에 맞게 수열 변환하기 3.js
@@ -1,5 +1,4 @@
 function solution(arr, k) {
     const isKEven = k % 2 === 0
-    const INIT = isKEven ? 0 : 1
-    return arr.map((ele) => isKEven ? ele + k : ele * k, INIT);
+    return arr.map((ele) => isKEven ? ele + k : ele * k);
 }
