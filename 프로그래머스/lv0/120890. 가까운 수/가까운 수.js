c8758eadb0af4a64fa119a0e1561c2c1f8114a78
blcklamb <whenucan35@gmail.com>
2023-09-02 15:32:54 +0900

    [lv0] Title: 가까운 수, -우수코드추가

--- 프로그래머스/lv0/120890. 가까운 수/가까운 수.js
+++ 프로그래머스/lv0/120890. 가까운 수/가까운 수.js
@@ -1,8 +1,14 @@
 function solution(array, n) {
-    array.sort((a, b) => {
-        if (Math.abs(a - n) < Math.abs(b - n)) return -1
-        if (Math.abs(a - n) > Math.abs(b - n)) return 1
-        return a - b
-    })
-    return array[0];
+  array.sort((a, b) => {
+    if (Math.abs(a - n) < Math.abs(b - n)) return -1;
+    if (Math.abs(a - n) > Math.abs(b - n)) return 1;
+    return a - b;
+  });
+  return array[0];
+}
+
+// 우수 코드
+function solution(array, n) {
+  array.sort((a, b) => Math.abs(n - a) - Math.abs(n - b) || a - b);
+  return array[0];
 }
