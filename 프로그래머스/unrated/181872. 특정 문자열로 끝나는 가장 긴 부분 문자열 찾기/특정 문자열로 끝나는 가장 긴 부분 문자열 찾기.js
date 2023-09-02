23f157809bf9b4de50545266545ee2a1e5a5c0b1
blcklamb <whenucan35@gmail.com>
2023-09-02 15:38:57 +0900

    [lv 0] Title: 특정 문자열로 끝나는 가장 긴 부분 문자열 찾기, -우수코드추가

--- 프로그래머스/unrated/181872. 특정 문자열로 끝나는 가장 긴 부분 문자열 찾기/특정 문자열로 끝나는 가장 긴 부분 문자열 찾기.js
+++ 프로그래머스/unrated/181872. 특정 문자열로 끝나는 가장 긴 부분 문자열 찾기/특정 문자열로 끝나는 가장 긴 부분 문자열 찾기.js
@@ -1,7 +1,10 @@
 function solution(myString, pat) {
-    for (let i = myString.length; i >= pat.length-1; i--){
-        if(myString.slice(i-pat.length, i) === pat){
-            return myString.slice(0, i)
-        }
+  for (let i = myString.length; i >= pat.length - 1; i--) {
+    if (myString.slice(i - pat.length, i) === pat) {
+      return myString.slice(0, i);
     }
+  }
 }
+
+// 우수 코드
+const solution = (str, pat) => str.substring(0, str.lastIndexOf(pat)) + pat;
