8f9155d7c2fc8a0e548b9350ab8d0ce8e1d8974a
blcklamb <whenucan35@gmail.com>
2023-09-02 15:40:00 +0900

    [level 0] Title: 한 번만 등장한 문자, -우수코드추가

--- 프로그래머스/lv0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.js
+++ 프로그래머스/lv0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.js
@@ -1,12 +1,19 @@
 function solution(s) {
-    let answer = '';
-    const charObj = {};
-    [...s].forEach(ele => {
-        if (charObj[ele]) charObj[ele] += 1
-        else charObj[ele] = 1
-    });
-    for (const [key, value] of Object.entries(charObj)){
-        if (value === 1) answer += key
-    }
-    return [...answer].sort().join('');
+  let answer = "";
+  const charObj = {};
+  [...s].forEach((ele) => {
+    if (charObj[ele]) charObj[ele] += 1;
+    else charObj[ele] = 1;
+  });
+  for (const [key, value] of Object.entries(charObj)) {
+    if (value === 1) answer += key;
+  }
+  return [...answer].sort().join("");
+}
+
+// 우수 코드
+function solution(s) {
+  let res = [];
+  for (let c of s) if (s.indexOf(c) === s.lastIndexOf(c)) res.push(c);
+  return res.sort().join("");
 }
