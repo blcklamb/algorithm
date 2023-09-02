f8883087b5eacc061303339fe55da9338c5ea249
blcklamb <whenucan35@gmail.com>
2023-09-02 21:34:32 +0900

    [lv 0] Title: 특별한 이차원 배열 2, -우수코드추가

--- 프로그래머스/unrated/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.js
+++ 프로그래머스/unrated/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.js
@@ -1,9 +1,19 @@
 function solution(arr) {
-    var answer = 1;
-    for (let i = 0; i < arr.length; i++){
-        for (let j = i+1; j < arr.length; j++){
-            if (arr[i][j] != arr[j][i]) {answer = 0; break;}
-        }
+  var answer = 1;
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i][j] != arr[j][i]) {
+        answer = 0;
+        break;
+      }
     }
-    return answer;
+  }
+  return answer;
+}
+
+// 우수 코드
+function solution(arr) {
+  return arr.every((r, i) => r.every((_, j) => arr[i][j] === arr[j][i]))
+    ? 1
+    : 0;
 }
