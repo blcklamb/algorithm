d6b2f03e54e5d43d390116e9fdc051cc914fa8bd
blcklamb <whenucan35@gmail.com>
2023-09-03 00:57:08 +0900

    [lv 0] Title: 0 떼기, -우수코드추가

--- 프로그래머스/unrated/181847. 0 떼기/0 떼기.js
+++ 프로그래머스/unrated/181847. 0 떼기/0 떼기.js
@@ -1,11 +1,16 @@
 function solution(n_str) {
-    let flag = 1
-    return [...n_str].filter(a => {
-        if (a === '0' & flag > 0){
-            flag += 1; 
-            return false
-        } 
-        flag = -1;
-        return true
-    }).join('');
+  let flag = 1;
+  return [...n_str]
+    .filter((a) => {
+      if ((a === "0") & (flag > 0)) {
+        flag += 1;
+        return false;
+      }
+      flag = -1;
+      return true;
+    })
+    .join("");
 }
+
+// 우수 코드
+const solution = (str) => String(Number(str));
