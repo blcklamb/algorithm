8dbf4d860ceb2a7c16119e9c7c6a5ccf668e7d1d
blcklamb <whenucan35@gmail.com>
2023-09-02 15:33:53 +0900

    [lv0] Title: 빈 배열에 추가, 삭제하기, -우수코드추가

--- 프로그래머스/unrated/181860. 빈 배열에 추가， 삭제하기/빈 배열에 추가， 삭제하기.js
+++ 프로그래머스/unrated/181860. 빈 배열에 추가， 삭제하기/빈 배열에 추가， 삭제하기.js
@@ -1,11 +1,22 @@
 function solution(arr, flag) {
-    var answer = [];
-    arr.forEach((ele, idx) => {
-        if (flag[idx]) {
-            for (let i = 0; i < ele*2; i++) answer.push(ele)
-        } else {
-            for (let i = 0; i < ele; i++)answer.pop()
-        }
-    })
-    return answer;
+  var answer = [];
+  arr.forEach((ele, idx) => {
+    if (flag[idx]) {
+      for (let i = 0; i < ele * 2; i++) answer.push(ele);
+    } else {
+      for (let i = 0; i < ele; i++) answer.pop();
+    }
+  });
+  return answer;
+}
+
+// 우수 코드
+function solution(arr, flag) {
+  return arr.reduce(
+    (prev, num, i) =>
+      flag[i]
+        ? [...prev, ...new Array(num * 2).fill(num)]
+        : prev.slice(0, -num),
+    []
+  );
 }
