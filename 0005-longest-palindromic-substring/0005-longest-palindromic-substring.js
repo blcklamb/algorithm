fc4d21b421b01fb6cb93fc00c44d8692158b4e60
Chaejung Kim <whenucan35@gmail.com>
2023-12-30 15:08:04 +0900

    Time: 702 ms (17.69%), Space: 48.7 MB (26.80%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.js
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.js
@@ -3,21 +3,19 @@
  * @return {string}
  */
 const isPalindrome = function(s) {
-    const sLen = s.length
-    for (let i = 0; i < (Math.floor(sLen-1) / 2); i++){
-        if (s[i] !== s[sLen-1-i]) return false
-    }
-    return true
+    return s.split('').reverse().join("") === s
 }
 var longestPalindrome = function(s) {
-    let answer = s[0]
     const sLen = s.length
-    for (let i = 0; i < sLen-1; i++){
-        for (let j = i+answer.length; j < sLen; j++){
-            if (isPalindrome(s.substring(i, j+1))){
-                answer = s.substring(i, j+1)
-            }
+    if (sLen === 1) return s
+
+    let [idx, len] = [0, 0] 
+    for (let i = 0; i < sLen; i++){
+        if (isPalindrome(s.substring(i-len, i+1))){
+            [idx, len] = [i-len, len+1]
+        } else if (i > len && isPalindrome(s.substring(i-len-1, i+1))){
+            [idx, len] = [i-len-1, len+2]
         }
     }
-    return answer
+    return s.substr(idx, len)
 };
